a5fb180ed317830a3ffde6bdc28580a00fd3a6c0
Dan Lauby <dmlauby@gmail.com>
2017-04-07 08:45:44 -0700

    Initialize project directories and setup firebase

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+exports var masterFirebaseConfig = {
+apiKey: "AIzaSyAC1vTLDgvxoiWOuDwsQI0Hqk3TI_PtsvE",
+authDomain: "club-manager-76d7f.firebaseapp.com",
+databaseURL: "https://club-manager-76d7f.firebaseio.com",
+projectId: "club-manager-76d7f",
+storageBucket: "club-manager-76d7f.appspot.com",
+messagingSenderId: "175997327862"
+}
