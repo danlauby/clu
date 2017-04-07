c12501b9b7bc6162b3b771f755fba17d0777492e
Dan Lauby <dmlauby@gmail.com>
2017-04-07 12:02:25 -0700

    Define  in model

--- src/app/player.model.ts
+++ src/app/player.model.ts
@@ -1,4 +1,4 @@
 export class Player {
-  // public $key: string;
+  public $key: string;
 constructor(public name: string, public age: string, public birthplace: string, public team: string, public position: string, public jerseyNumber: string, public image: string, public goalsThisSeason: number) {}
 }
