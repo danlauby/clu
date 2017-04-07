d4be95d7fce010d0d229aa7e27887ad68762ca24
Dan Lauby <dmlauby@gmail.com>
2017-04-07 11:58:24 -0700

    Add player/:id path to router

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListPlayersComponent } from './list-players/list-players.component';
+import { DetailComponent } from './detail/detail.component';
 
 const appRoutes: Routes = [
   {
     path:'',
     component: ListPlayersComponent
+  },
+  {
+    path: 'player/:id',
+    component: DetailComponent
   }
  ];
 
