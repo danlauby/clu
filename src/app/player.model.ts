73beee609b3d48a19504fd4388efab115da4d458
Dan Lauby <dmlauby@gmail.com>
2017-04-07 10:59:15 -0700

    Add age, jerseyNumber, birthplace properties to players

--- src/app/player.model.ts
+++ src/app/player.model.ts
@@ -1,4 +1,4 @@
 export class Player {
   // public $key: string;
-constructor(public name: string, public team: string, public position: string, public image: string, public goalsThisSeason: string,) {}
+constructor(public name: string, public age: string, public birthplace: string, public team: string, public position: string, public jerseyNumber: string, public image: string, public goalsThisSeason: number) {}
 }
